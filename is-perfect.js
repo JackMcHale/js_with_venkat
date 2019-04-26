a48661a1fbea442df1f3659b0b3f88a4cf457062
JackMcHale <jack.mchale@gmail.com>
2019-04-25 22:06:08 -0400

    Chapter 1 changes

---
+++ is-perfect.js
@@ -0,0 +1,14 @@
+'use strict'
+var isPerfect = function (number) {
+    var sumOfFactors = 0;
+    for (let index = 1; index <= number; index++) {
+        if (number % index == 0) {
+            sumOfFactors += index;
+        }
+    }
+    return sumOfFactors === number * 2;
+}
+
+for (let i = 1; i <= 10; i++) {
+    process.stdout.write('is ' + i + ' perfect?: ' + isPerfect(i) + '\n');
+}
